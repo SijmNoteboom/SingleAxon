bb3d5d4e803236528f8669d333aea3b708da059a
SijmNoteboom <sijmnoteboom95@gmail.com>
2019-11-20 15:08:30 +0100

    finished random points and plot next: cumulative function

--- TestEnvironment.py
+++ TestEnvironment.py
@@ -1,4 +1,4 @@
-class car(): 
+class Car:
       
     # init method or constructor 
     def __init__(self, model, color): 
@@ -7,4 +7,19 @@ class car():
           
     def show(self): 
         print("Model is", self.model ) 
-        print("color is", self.color ) 
+        print("color is", self.color )
+
+    # both objects have different self which
+
+
+# contain their attributes
+audi = Car("audi a4", "blue")
+ferrari = Car("ferrari 488", "green")
+
+audi.show()  # same output as car.show(audi)
+ferrari.show()  # same output as car.show(ferrari)
+
+
+# Behind the scene, in every instance method
+# call, python sends the instances also with
+# that method call like car.show(audi)
--- simulatedAxon.py
+++ simulatedAxon.py
@@ -1,12 +1,16 @@
 import random as rand
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame(columns=['stimulus'])
 for i in range(100):
     df = df.append({'stimulus': rand.uniform(1, 3)}, ignore_index=True)
 print(df)
 
-df = df.append(self, )
+response = np.random.randint(2, size=100) * 2 + np.random.normal(0, .1, 100)
 
-x = 3
+df['response'] = response
+
+plt.scatter(df['stimulus'], df['response'])
+plt.show()
